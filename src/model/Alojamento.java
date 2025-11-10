24bfd074d843bc71dc785c6a56498d10ba8d4e3e
Joaquim Segunda <joelpereirasegunda@gmail.com>
2025-11-10 15:42:31 +0000

    Finished adding logic to alojamento

--- src/model/Alojamento.java
+++ src/model/Alojamento.java
@@ -2,23 +2,46 @@ package model;
 
 public class Alojamento {
     private int id;
+    private String nome;
     private String morada;
     private int capacidade;
     private String estado; //EX: "pendente","Aprovado","recusado","incompleto"
 
-    public Alojamento(String morada, int capacidade){
+    public Alojamento(String nome, String morada, int capacidade){
+        this.nome = nome;
         this.morada = morada;
         this.capacidade = capacidade;
         this.estado = "Pendente"; //Estado inicial
     }
 
     // Construtor ao ler a base de dados
-    public Alojamento(int id, String morada, int capacidade, String estado){
+    public Alojamento(int id,String nome, String morada, int capacidade, String estado){
         this.id = id;
+        this.nome = nome;
         this.morada = morada;
         this.capacidade = capacidade;
-        this.estado = "Pendente"; //Estado inicial
+        this.estado = estado;
     }
 
     // Getters e Setters
+    public int getId(){return id; }
+    public void setId(int id){this.id = id; }
+
+    public String getNome(){return nome; }
+    public void setNome(String Nome){this.nome = Nome; }
+
+    public String getMorada(){return morada; }
+    public void setMorada(String Morada){this.morada = Morada; }
+
+    public int getCapacidade(){return capacidade; }
+    public void getCapacidade(int Capacidade){this.capacidade = Capacidade; }
+
+    public String getEstado(){return estado;}
+    public void getEstado(String Estado){this.estado = Estado;}
+
+    @Override
+    public String toString(){
+        return "Alojamento "+nome+"\nId: "+id+"\nMorada: "+morada+"\nEstado: "+estado;
+    }
+
 }
