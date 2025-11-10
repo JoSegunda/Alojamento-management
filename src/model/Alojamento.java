ca4ca510544c84057e7c1ba22db5899aef70179d
Joaquim Segunda <joelpereirasegunda@gmail.com>
2025-11-10 15:30:48 +0000

    added logic to alojamento

--- src/model/Alojamento.java
+++ src/model/Alojamento.java
@@ -1,4 +1,24 @@
 package model;
 
 public class Alojamento {
+    private int id;
+    private String morada;
+    private int capacidade;
+    private String estado; //EX: "pendente","Aprovado","recusado","incompleto"
+
+    public Alojamento(String morada, int capacidade){
+        this.morada = morada;
+        this.capacidade = capacidade;
+        this.estado = "Pendente"; //Estado inicial
+    }
+
+    // Construtor ao ler a base de dados
+    public Alojamento(int id, String morada, int capacidade, String estado){
+        this.id = id;
+        this.morada = morada;
+        this.capacidade = capacidade;
+        this.estado = "Pendente"; //Estado inicial
+    }
+
+    // Getters e Setters
 }
