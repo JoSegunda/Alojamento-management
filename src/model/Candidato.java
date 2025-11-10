b3945e949070e27d195b2994646cac4a686505e4
Joaquim Segunda <joelpereirasegunda@gmail.com>
2025-11-10 15:52:34 +0000

    added logic to alojamento

--- src/model/Candidato.java
+++ src/model/Candidato.java
@@ -1,4 +1,42 @@
 package model;
 
 public class Candidato {
+    private int id;
+    private String nome;
+    private String email;
+    private String telefone;
+
+    // Construtor para novo candidato
+    public Candidato(String nome, String email, String telefone){
+        this.nome = nome;
+        this.email = email;
+        this.telefone = telefone;
+    }
+
+    //Construtor completo
+    public Candidato(int id, String nome, String email, String telefone){
+        this.id = id;
+        this.nome = nome;
+        this.email = email;
+        this.telefone = telefone;
+    }
+
+    // Getters e Setters
+    public int getId() { return id; }
+    public void setId(int id) { this.id = id; }
+
+    public String getNome() { return nome; }
+    public void setNome(String nome) { this.nome = nome; }
+
+    public String getEmail() { return email; }
+    public void setEmail(String email) { this.email = email; }
+
+    public String getTelefone() { return telefone; }
+    public void setTelefone(String telefone) { this.telefone = telefone; }
+
+    @Override
+    public String toString(){
+        return "ID do candidato\n"+id+"\nNome do candidato: "+nome+"\nemail: "+email;
+    }
+
 }
