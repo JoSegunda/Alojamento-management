7abb99db4eb2be1d223de3762e84f14b67825560
Joaquim Segunda <joelpereirasegunda@gmail.com>
2025-11-11 11:39:55 +0000

    Added logic to the database.

--- src/repository/DatabaseConnection.java
+++ src/repository/DatabaseConnection.java
@@ -1,4 +1,18 @@
 package repository;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class DatabaseConnection {
+    private static final String URL = "jdbc:postgresql://localhost:5432/alojamento_db";
+    private static final String USER = "alojamento_admin"
+    private static final String PASSWORD = "12345";
+
+    private static Connection connection = null;
+
+    private DatabaseConnection(){
+        // Construtor privado para impedir instanciação externa
+    }
+
 }
