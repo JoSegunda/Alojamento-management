b5ef90f98e90ccbad8a1d00e9e26d6b92a4d3ff2
Joaquim Segunda <joelpereirasegunda@gmail.com>
2025-11-11 11:51:57 +0000

    Added a DatabaseConnection logic

--- src/repository/DatabaseConnection.java
+++ src/repository/DatabaseConnection.java
@@ -6,7 +6,7 @@ import java.sql.SQLException;
 
 public class DatabaseConnection {
     private static final String URL = "jdbc:postgresql://localhost:5432/alojamento_db";
-    private static final String USER = "alojamento_admin"
+    private static final String USER = "alojamento_admin";
     private static final String PASSWORD = "12345";
 
     private static Connection connection = null;
@@ -20,5 +20,33 @@ public class DatabaseConnection {
      * @throws SQLException Se ocorrer um erro de conexão.
      */
 
+    public static Connection getConnection() throws SQLException{
+        if (connection == null || connection.isClosed()){
+            try {
+                // 1. Carregar o driver JDBC
+
+                // 2. Estabelecer a conexão
+                connection = DriverManager.getConnection(URL,USER,PASSWORD);
+                System.out.println("Conexão estabelecida com sucesso.");
+            }catch (SQLException e){
+                System.err.println("Falha ao conectar ao PostgreSQL: "+ e.getMessage());
+                throw e;
+            }
+        }
+        return connection;
+    }
+
+    // Fechar a conexão
+    public static void closeConnection(){
+        if (connection !=null){
+            try {
+                connection.close();
+                connection = null;
+                System.out.println("Conexão com postgres encerrada.");
+            }catch (SQLException e){
+                System.err.println("Erro ao fechar a conexão: "+ e.getMessage());
+            }
+        }
+    }
 
 }
