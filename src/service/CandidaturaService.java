77322ee3c588805eea474649574d63ac1e1cb281
Joaquim Segunda <joelpereirasegunda@gmail.com>
2025-11-12 13:41:26 +0000

    added import statements

--- src/service/CandidaturaService.java
+++ src/service/CandidaturaService.java
@@ -1,4 +1,14 @@
 package service;
 
+import model.Candidatura;
+import model.Candidatura.EstadoCandidatura;
+import model.Alojamento;
+import model.Candidato;
+import repository.CandidaturaRepository;
+import repository.AlojamentoRepository;
+import repository.CandidatoRepository;
+import java.sql.SQLException;
+import java.util.Optional;
+
 public class CandidaturaService {
 }
