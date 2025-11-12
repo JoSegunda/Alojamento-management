fb846b375a6b0a40751a7e3f9e25300f1e02ab04
Joaquim Segunda <joelpereirasegunda@gmail.com>
2025-11-12 12:46:16 +0000

    Added import statements

--- src/repository/CandidaturaRepository.java
+++ src/repository/CandidaturaRepository.java
@@ -1,4 +1,9 @@
 package repository;
 
+import model.Candidatura;
+import model.Candidatura.EstadoCandidatura; // Importar o ENUM
+import java.sql.*;
+import java.util.Optional;
+
 public class CandidaturaRepository {
 }
