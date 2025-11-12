0dcff74b1ab33dd10bb1fd168bfd6cb9f23818d1
Joaquim Segunda <joelpereirasegunda@gmail.com>
2025-11-12 12:38:33 +0000

    Added mapResultSetToCandidato method

--- src/repository/CandidatoRepository.java
+++ src/repository/CandidatoRepository.java
@@ -7,4 +7,18 @@ import java.sql.*;
 import java.util.Optional;
 
 public class CandidatoRepository {
+
+    // M auxiliar para mapear um ResultSet para um objeto Candidato
+    private Candidato mapResultSetToCandidato(ResultSet rs) throws SQLException {
+        return new Candidato(
+                rs.getInt("id"),
+                rs.getString("nome"),
+                rs.getString("email"),
+                rs.getString("telefone"),
+                Sexo.valueOf(rs.getString("sexo").toUpperCase()),
+                rs.getString("curso"),
+                rs.getDate("data_registo").toLocalDate(), // Converte SQL Date para LocalDate
+                EstadoCandidato.valueOf(rs.getString("estado").toUpperCase())
+        );
+    }
 }
