0cb61ba72973c27182d3b432956b1d6c0f50c6d0
Joaquim Segunda <joelpereirasegunda@gmail.com>
2025-11-12 12:21:01 +0000

    Adicionado método mapResultSetToAlojamento

--- src/repository/AlojamentoRepository.java
+++ src/repository/AlojamentoRepository.java
@@ -1,4 +1,22 @@
 package repository;
 
+import model.Alojamento;
+import model.Alojamento.EstadoAlojamento;
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+
 public class AlojamentoRepository {
+
+    // M auxiliar para mapear um ResultSet para um objeto Alojamento
+    private Alojamento mapResultSetToAlojamento(ResultSet rs) throws SQLException{
+        return new Alojamento(
+                rs.getInt("id"),
+                rs.getString("nome"),
+                rs.getString("cidade"),
+                rs.getBoolean("active"),
+                rs.getInt("Capacidade"),
+                EstadoAlojamento.valueOf(rs.getString("estado").toUpperCase())
+        );
+    }
 }
