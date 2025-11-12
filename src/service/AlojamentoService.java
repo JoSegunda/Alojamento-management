5dc9ee2143acaff6de58045fcf991a001268cf5e
Joaquim Segunda <joelpereirasegunda@gmail.com>
2025-11-12 13:19:36 +0000

    Added import statements

--- src/service/AlojamentoService.java
+++ src/service/AlojamentoService.java
@@ -1,4 +1,10 @@
 package service;
 
+import model.Alojamento;
+import model.Alojamento.EstadoAlojamento;
+import repository.AlojamentoRepository;
+import java.sql.SQLException;
+import java.util.List;
+
 public class AlojamentoService {
 }
