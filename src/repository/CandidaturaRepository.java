ae1d59e95bd81237661e93c3db4654775cc50a76
Joaquim Segunda <joelpereirasegunda@gmail.com>
2025-11-12 12:46:55 +0000

    Added method to map a resultset to the object candidatura

--- src/repository/CandidaturaRepository.java
+++ src/repository/CandidaturaRepository.java
@@ -6,4 +6,14 @@ import java.sql.*;
 import java.util.Optional;
 
 public class CandidaturaRepository {
+    // M auxiliar para mapear um ResultSet para um objeto Candidatura
+    private Candidatura mapResultSetToCandidatura(ResultSet rs) throws SQLException {
+        return new Candidatura(
+                rs.getInt("id"),
+                rs.getInt("alojamento_id"),
+                rs.getInt("candidato_id"),
+                rs.getDate("data_candidatura").toLocalDate(),
+                EstadoCandidatura.valueOf(rs.getString("estado").toUpperCase())
+        );
+    }
 }
